38f2ba45517fb87520105605ff938d7e2d1934de
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-29 21:55:31 +0300

    created spinner component

--- src/components/utility/index.ts
+++ src/components/utility/index.ts
@@ -1,7 +1,9 @@
 import { App } from "vue";
+import pSpinner from "./p-spinner.vue";
 import pButton from "./p-button.vue";
 
 export default (vueApp: App<Element>) => {
+  vueApp.component("p-spinner", pSpinner);
   vueApp.component("p-button", pButton);
 
   return vueApp;
---
+++ src/components/utility/p-spinner.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: "primary",
+    validator: (value: string) => {
+      const validTypes = ["primary", "secondary"];
+      const message = "maxPagesShown attribute must be greater than 0.";
+
+      if (!validTypes.includes(value)) {
+        console.error(message);
+        throw new TypeError(message);
+      }
+      return true;
+    },
+  },
+  size: {
+    type: String,
+    default: "md",
+    validator: (value: string) => {
+      const validTypes = ["sm", "md", "lg"];
+      const message = "maxPagesShown attribute must be greater than 0.";
+
+      if (!validTypes.includes(value)) {
+        console.error(message);
+        throw new TypeError(message);
+      }
+      return true;
+    },
+  },
+});
+</script>
+
+<template>
+  <div role="status">
+    <div
+      style="border-top-color: transparent"
+      :class="['spinner', `spinner-${size}`, `spinner-${type}`]"
+    ></div>
+
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
+
+<style scoped></style>
