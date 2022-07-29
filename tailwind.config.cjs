7c86492a940558268316182be9d9eaf52169f122
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-29 21:54:27 +0300

    cleanup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,32 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    // fontFamily: {
-    //   mulish: ["Mulish", "Sans-serif"],
-    // },
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
       xl: "1440px",
     },
-    // fontSize: {
-    //   xs: "10px",
-    //   sm: "12px",
-    //   base: "14px",
-    //   lg: "16px",
-    //   xl: "24px",
-    //   "2xl": "32px",
-    // },
     colors: {
-      primary: "#002D63",
-      secondary: "#00A1E5",
-      background: "#F5F5F5",
-      danger: "#D80000",
-      warning: "#EE8F13",
-      success: "#019501",
+      transparent: "transparent",
+      black: {
+        300: "#6b6b6b",
+        500: "#4b4b4b",
+        700: "#2e2e2e",
+        900: "#000000",
+      },
       white: "#FFFFFF",
-      black: "#000000",
     },
     extend: {},
   },
