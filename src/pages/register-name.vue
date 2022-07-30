d981a63d5ca0cb701d3453ff9d809e3f36e1aa67
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-30 23:44:38 +0300

    added register page

---
+++ src/pages/register-name.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center h-[100vh] flex-col gap-8">
+    <input
+      class="input-primary | w-[90%] h-[60px] sm:w-[300px]"
+      type="text"
+      placeholder="Enter your name"
+    />
+    <p-button class="w-[90%] h-[60px] sm:w-[300px]" type="primary"
+      >Login</p-button
+    >
+  </div>
+</template>
