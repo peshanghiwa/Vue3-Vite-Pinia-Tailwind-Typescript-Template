48002de56c145144d0530ab7ac678ff8a829a262
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-30 23:57:48 +0300

    created header component

---
+++ src/components/layout/header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <head class="h-[90px] block border-black-900 border-b-[1px] px-[60px]">
+    <nav class="w-full h-full flex justify-between items-center">
+      <ul class="flex gap-10 text-lg">
+        <li>
+          <router-link to="/register-name">Country Profile</router-link>
+        </li>
+        <li>
+          <router-link to="/select-country">University List</router-link>
+        </li>
+      </ul>
+      <section class="flex items-center gap-10 text-lg">
+        <p>Hello Mr. Username</p>
+        <p-button class="w-[150px] h-[40px]" type="primary">Logout</p-button>
+      </section>
+    </nav>
+  </head>
+</template>
