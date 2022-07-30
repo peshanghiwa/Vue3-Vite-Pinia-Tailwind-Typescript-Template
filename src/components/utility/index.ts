9624e9ba0ce0ad91e479c2156d5e7dd00e6e8ae9
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-30 23:44:14 +0300

    cleanup

--- src/components/utility/index.ts
+++ src/components/utility/index.ts
@@ -5,6 +5,4 @@ import pButton from "./p-button.vue";
 export default (vueApp: App<Element>) => {
   vueApp.component("p-spinner", pSpinner);
   vueApp.component("p-button", pButton);
-
-  return vueApp;
 };
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
+import RegisterName from "./pages/register-name.vue";
+import SelectCountry from "./pages/select-country.vue";
 
 const routes = [
   { path: "/", component: Home },
+  {
+    path: "/register-name",
+    component: RegisterName,
+  },
+  {
+    path: "/select-country",
+    component: SelectCountry,
+  },
 ];
 
 export default createRouter({
