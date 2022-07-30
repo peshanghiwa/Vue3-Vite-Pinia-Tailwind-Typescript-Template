74a9c68cf9df02bd8189c7de8bd2608da255c854
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-30 23:44:45 +0300

    added select country page

---
+++ src/pages/select-country.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { reactive, toRefs } from "vue";
+
+const data = reactive({
+  country: null,
+});
+const { country } = toRefs(data);
+const options = ["Afghanistan", "Albania", "Algeria", "Andorra"];
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-[100vh] flex-col gap-8">
+    <v-select
+      :options="options"
+      v-model="country"
+      class="w-[90%] h-[60px] sm:w-[300px]"
+      placeholder="Enter your name"
+    />
+    <p-button class="w-[90%] h-[60px] sm:w-[300px]" type="primary"
+      >Proceed</p-button
+    >
+  </div>
+</template>
