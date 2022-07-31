c26d11ed52351e4a49f61d5475706d8fd8731698
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-08-01 02:36:49 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,8 +9,11 @@ module.exports = {
       xl: "1440px",
     },
     colors: {
+      red: "red",
       transparent: "transparent",
       black: {
+        100: "#fcfcfc",
+        200: "#ececec",
         300: "#6b6b6b",
         500: "#4b4b4b",
         700: "#2e2e2e",
