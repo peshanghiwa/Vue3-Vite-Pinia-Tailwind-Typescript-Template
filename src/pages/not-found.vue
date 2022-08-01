2c6ddb25148cde9707e1b4f1709b47e5ecf8aa18
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-08-02 01:06:50 +0300

    general cleanup

--- src/pages/not-found.vue
+++ src/pages/not-found.vue
@@ -4,7 +4,7 @@ const { isLoggedIn } = useAuthStore();
 
 <template>
   <main
-    class="main-layout | flex items-center flex-col mt-[20px] md:mt-[100px]"
+    class="main-layout | flex items-center flex-col mt-[20px] md:mt-[100px] text-center"
   >
     <h1 class="text-[40px] font-bold">404, Page not found.</h1>
     <router-link to="/" class="underline"> Go to home page </router-link>
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,4 +1,3 @@
-import { ref, Ref } from "vue";
 import axios from "axios";
 // normally we would have a more advanced configurations, but for this project as we only use get requests, we can just keep it basic
 export default axios.create({
