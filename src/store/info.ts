6b8a20acee466367b7e58146245a9bcdf36cd598
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-08-01 18:07:02 +0300

    added info store

---
+++ src/store/info.ts
@@ -0,0 +1,16 @@
+import { Ref, ref, computed } from "vue";
+import { defineStore } from "pinia";
+import { Country } from "../types/info";
+
+export const useInfoStore = defineStore("info", () => {
+  // State Properties
+  const country = ref(null) as Ref<Country>;
+
+  // Getters
+  const selectedCountry = computed(() => country.value);
+
+  // Actions
+  const setCountry = (value: Country) => (country.value = value);
+
+  return { selectedCountry, setCountry };
+});
